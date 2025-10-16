aa2a105ebf528a97e67041748f744914d9da0401
Hardian E.C <dakochan64@gmail.com>
2025-10-16 21:31:09 +0700

    memakai dom untuk menampilkan ke page bukan di console lagi

--- index.js
+++ index.js
@@ -19,23 +19,49 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
-    console.log(humanChoice + ' vs ' + computerChoice)
+    const whoVsWho = document.querySelector('#whovswho')
+    let who = humanChoice + ' vs ' + computerChoice
+    whoVsWho.insertAdjacentText('beforeend', who)
     if(humanChoice == computerChoice){
-        return console.log('Result is Draw')
+        return 'Result is Draw'
     }
     else if(humanChoice == 'rock' && computerChoice == 'scissor'){
-        return console.log('Human WINN')
+        return 'Human WINN'
     }else if(humanChoice == 'rock' && computerChoice == 'paper'){
-        return console.log('Human LOSE')
+        return 'Human LOSE'
     }else if(humanChoice == 'paper' && computerChoice == 'rock'){
-        return console.log('Human WINN')
+        return 'Human WINN'
     }else if(humanChoice == 'paper' && computerChoice == 'scissor'){
-        return console.log('Human LOSE')
+        return 'Human LOSE'
     }else if(humanChoice == 'scissor' && computerChoice == 'paper'){
-        return console.log('Human WINN')
+        return 'Human WINN'
     }else if(humanChoice == 'scissor' && computerChoice == 'rock'){
-        return console.log('Human LOSE')
+        return 'Human LOSE'
     }
 }
 
-playRound(getHumanChoice() , getComputerChoice());
+// playRound(getHumanChoice() , getComputerChoice());
+
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const result = document.querySelector('#result')
+const whoVsWho = document.querySelector('#whovswho')
+
+rock.addEventListener("click", () => {
+    result.textContent = ""
+    whoVsWho.textContent = ""
+    result.insertAdjacentText('beforeend', playRound('rock', getComputerChoice()))
+})
+
+paper.addEventListener("click", () => {
+    result.textContent = ""
+    whoVsWho.textContent = ""
+    result.insertAdjacentText('beforeend', playRound('paper', getComputerChoice()))
+})
+
+scissor.addEventListener("click", () => {
+    result.textContent = ""
+    whoVsWho.textContent = ""
+    result.insertAdjacentText('beforeend', playRound('scissor', getComputerChoice()))
+})
